4e6e790e82e5552e3aebd818445f327675d8d339
JoseS-Dev <joseasantana05@gmail.com>
2026-09-22 12:58:31 -0400

    feat: :sparkles: Enhance Swagger configuration with JWT security definition and API documentation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using DotNetEnv;
+using Microsoft.OpenApi.Models;
 using TodoList_NET.Extensions;
 using TodoList_NET.Services.UserServices;
 using TodoList_NET.Services.TaskServices;
@@ -12,7 +13,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 1. Configuración de Servicios
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Title = "TodoListNet API", 
+        Version = "v1" 
+    });
+    c.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Description = "Ingrese 'Bearer' seguido de un espacio y luego su token JWT."
+    });
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddControllers();
 
 // Inyección de dependencias personalizadas
--- Usings.cs
+++ Usings.cs
@@ -5,3 +5,4 @@ global using System.IdentityModel.Tokens.Jwt;
 global using Microsoft.IdentityModel.Tokens;
 global using BCrypt.Net;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Authorization;
